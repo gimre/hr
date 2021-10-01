556d7988271fc8b160afd2c3c5ac2c2dd2b4fd68
Gabriel Imre <imregabriel@gmail.com>
2021-10-01 16:28:37 +0300

    Update plot.js

--- plot.js
+++ plot.js
@@ -1,34 +1,26 @@
+void async function () {
+  const data = await fetch('./data.json')
+    .then(r => r.json())
+  
+  window.plot = new uPlot({
+    id: "chart1",
+    class: "my-chart",
+    width: window.innerWidth,
+    height: window.innerHeight,
+    series: [
+      {},
+      {
+        spanGaps: false,
+        label: "HR",
+        stroke: "red",
+        width: 1 / devicePixelRatio,
+      }
+    ],
+  }, data, document.body)
+}()
+
 const data = [
   [1546300800, 1546387200],    // x-values (timestamps)
   [        35,         71],    // y-values (series 1)
   [        90,         15],    // y-values (series 2)
 ]
-
-let opts = {
-  title: "My Chart",
-  id: "chart1",
-  class: "my-chart",
-  width: 800,
-  height: 600,
-  series: [
-    {},
-    {
-      // initial toggled state (optional)
-      show: true,
-
-      spanGaps: false,
-
-      // in-legend display
-      label: "RAM",
-      value: (self, rawValue) => "$" + rawValue.toFixed(2),
-
-      // series style
-      stroke: "red",
-      width: 1,
-      fill: "rgba(255, 0, 0, 0.3)",
-      dash: [10, 5],
-    }
-  ],
-}
-
-let uplot = new uPlot(opts, data, document.body)
